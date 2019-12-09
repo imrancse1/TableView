243a97a64b6ceb80c6f70899fd7563151ea9e1ff
imrancse1 <imran.cse.project@gmail.com>
2019-12-09 02:31:09 -0800

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Zakaria on 8/12/19.
+//  Copyright © 2019 Apple soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tab: UITableView!
+    
+    @IBOutlet weak var addText: UITextField!
+    
+    
+    var data = ["Dog", "Cat", "Mouse", "Fish"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(data)
+        return data.count
+          
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = data[indexPath.row]
+        print(cell)
+
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addData(_ sender: UIButton) {
+      
+        data.append(addText.text!)
+        tab.reloadData()
+      
+      
+        //print("prince")
+       
+    }
+    
+}
+
